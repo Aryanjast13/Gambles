1272b7132f21ea8be1cb1d573bb21cdd22c0a1e8
Aryanjast13 <aryanjastaj05@gmail.com>
2025-07-04 23:11:13 +0530

    nodemailer config edit

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,13 +1,11 @@
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false, // true for port 465, false for other ports
+    service: "gmail",
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
     },
 });
 
-export default transporter
+export default transporter;
