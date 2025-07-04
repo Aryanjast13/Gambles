f209e731636a213d31790e5faeb8db76afac3bf1
Aryanjast13 <aryanjastaj05@gmail.com>
2025-07-05 00:06:34 +0530

    fix naviagtion

--- client/src/components/OauthSuccess.jsx
+++ client/src/components/OauthSuccess.jsx
@@ -1,14 +1,26 @@
+import { useUser } from "@/contexts/UserContext";
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export default function OauthSuccess() {
     const navigate = useNavigate();
+    const { fetchUser } = useUser();
 
     useEffect(() => {
-        toast.success("Login successful!");
+        
         navigate("/"); // or navigate to dashboard or profile
     }, [navigate]);
+
+    useEffect(() => {
+        const completeLogin = async () => {
+            toast.success("Login successful!");
+          await fetchUser(); // sets the user after cookies are set
+          navigate("/dashboard"); // or wherever you want
+        };
+    
+        completeLogin();
+      }, []);
 
     return null;
 }
